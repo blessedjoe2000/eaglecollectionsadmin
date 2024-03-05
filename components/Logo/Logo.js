e9bf0f0d91ba683b51a91bde9e48dc1d39a3c0d2
Joseph Ugiagbe <joetalker@Josephs-MBP.attlocal.net>
2024-03-05 17:41:35 -0600

    logo changed

--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import logo from "@/public/images/eaglecollectionslogowhite.png";
+import logo from "@/public/images/eaglecollectionslogo.png";
 import Image from "next/image";
 
 export default function Logo() {
