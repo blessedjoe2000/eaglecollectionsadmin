efca9fc15296f6f17748d3ffc8e5b6c899610c67
Joseph Ugiagbe <joetalker@Josephs-MBP.attlocal.net>
2024-03-05 12:18:47 -0600

    code wraped in trycatch

--- app/api/products/pages/[page]/route.js
+++ app/api/products/pages/[page]/route.js
@@ -8,18 +8,18 @@ export async function GET(req, ctx) {
   await mongooseConnect();
   await isAdminRequest();
 
-  let { page } = ctx.params;
-  page = parseInt(page);
-  const itemLimit = 50;
-
-  const skipPage = (page - 1) * itemLimit;
-
-  const allProducts = await Product.find()
-    .sort({ updatedAt: -1 })
-    .limit(itemLimit)
-    .skip(skipPage);
-
   try {
+    let { page } = ctx.params;
+    page = parseInt(page);
+    const itemLimit = 50;
+
+    const skipPage = (page - 1) * itemLimit;
+
+    const allProducts = await Product.find()
+      .sort({ updatedAt: -1 })
+      .limit(itemLimit)
+      .skip(skipPage);
+
     return new Response(JSON.stringify(allProducts), { status: 200 });
   } catch (error) {
     return new Response(JSON.stringify(error.message), { status: 500 });
